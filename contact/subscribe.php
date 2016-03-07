6aa356a4db23a3736141664682b28c64df5dd97f
jawahar <jawahit@gmail.com>
2016-03-08 02:03:17 +0530

    mail testing1

--- contact/subscribe.php
+++ contact/subscribe.php
@@ -6,12 +6,12 @@
 	$subject = $_REQUEST[ 'contact-subject' ];
 	$message = $_REQUEST[ 'contact-message' ];
 	$mail_subject = $subject . "( Customer Contact )";
-	$message = "Name: ".$name." || Email: ".$email." || Subject: ".$subject." || Message: ".$message;
-	//$merge_vars = array('FNAME'=>$_POST["fname"], 'LNAME'=>$_POST["lname"]);
+	// $message = "Name: ".$name." || Email: ".$email." || Subject: ".$subject." || Message: ".$message;
+	$merge_vars = array('FNAME'=>$_REQUEST[ 'contact-name' ], 'LNAME'=>$_REQUEST[ 'contact-name' ]);
 
 	// Submit subscriber data to MailChimp
 	// For parameters doc, refer to: http://apidocs.mailchimp.com/api/1.3/listsubscribe.func.php
-	$retval = $api->listSubscribe( '6784b478298a46ab36a612aa46b8b62d-us1','831ab6ad71', $email, $message, 'html', false, true ,true, true);
+	$retval = $api->listSubscribe( '6784b478298a46ab36a612aa46b8b62d-us1','831ab6ad71', $email, $merge_vars, 'html', false, true ,true, true);
 
 	if ($api->errorCode){
 		echo "Error in sending message !!! Please try again";
